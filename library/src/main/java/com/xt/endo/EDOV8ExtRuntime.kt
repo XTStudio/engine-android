d4ed7fd585c2249b45ffd215688e3cd5764add13
PonyCui <cuis@vip.qq.com>
2018-08-09 11:14:41 +0800

    Bug-fixed: could not use V8 as Map key, if V8 released, app crashed.

--- library/src/main/java/com/xt/endo/EDOV8ExtRuntime.kt
+++ library/src/main/java/com/xt/endo/EDOV8ExtRuntime.kt
@@ -3,6 +3,7 @@ package com.xt.endo
 import com.eclipsesource.v8.V8
 import com.eclipsesource.v8.V8Object
 import com.eclipsesource.v8.V8Value
+import com.xt.jscore.JSContext
 import java.lang.ref.WeakReference
 import java.util.*
 import kotlin.collections.HashMap
@@ -11,6 +12,7 @@ private val refMapping: WeakHashMap<Any, String> = WeakHashMap()
 private val weakValueRefMapping: HashMap<String, WeakReference<Any>> = HashMap()
 
 private fun Any.edo_objectRef(): String {
+    if (this is V8Value) { return "" }
     return refMapping[this] ?: kotlin.run {
         val objectRef = UUID.randomUUID().toString()
         refMapping[this] = objectRef
@@ -22,22 +24,24 @@ private fun Any.edo_objectRef(): String {
 /**
  * Created by cuiminghui on 2018/7/17.
  */
-class EDOV8ExtRuntime(val value: WeakReference<V8>) {
+class EDOV8ExtRuntime(val value: WeakReference<JSContext>) {
 
     private val soManagedValue: WeakHashMap<Any, V8Object> = WeakHashMap()
 
     fun storeScriptObject(anObject: Any, scriptObject: V8Object) {
+        if (anObject is V8Value) { return }
         soManagedValue[anObject] = scriptObject.twin().setWeak() as? V8Object
     }
 
     fun createMetaClass(anObject: Any): V8Value {
         try {
             val runtime = value.get() ?: return V8.getUndefined()
-            return runtime.executeObjectScript("new _EDO_MetaClass('${anObject::class.java.name}', '${anObject.edo_objectRef()}')")
+            return runtime.runtime.executeObjectScript("new _EDO_MetaClass('${anObject::class.java.name}', '${anObject.edo_objectRef()}')")
         } catch (e: Exception) { return V8.getUndefined() }
     }
 
     fun scriptObjectWithJavaObject(anObject: Any, createdIfNeed: Boolean = true, initializer: EDOCallback? = null): V8Value {
+        if (anObject is V8Value) { return anObject }
         this.soManagedValue[anObject]?.takeIf { !it.isReleased }?.let { return it }
         if (!createdIfNeed) { return V8.getUndefined() }
         val context = this.value.get() ?: return V8.getUndefined()
@@ -61,12 +65,12 @@ class EDOV8ExtRuntime(val value: WeakReference<V8>) {
         }
         target?.let { target ->
             initializer?.let { initializer ->
-                val objectMetaClass = context.executeObjectScript("new _EDO_MetaClass('${target.name}', '${anObject.edo_objectRef()}')")
+                val objectMetaClass = context.runtime.executeObjectScript("new _EDO_MetaClass('${target.name}', '${anObject.edo_objectRef()}')")
                 val scriptObject = initializer.invokeAndReturnV8Object(objectMetaClass) ?: return V8.getUndefined()
                 soManagedValue[anObject] = scriptObject.twin().setWeak() as? V8Object
                 return scriptObject
             } ?: kotlin.run {
-                val scriptObject = context.executeObjectScript("new ${target.name}(new _EDO_MetaClass('${target.name}', '${anObject.edo_objectRef()}'))")
+                val scriptObject = context.runtime.executeObjectScript("new ${target.name}(new _EDO_MetaClass('${target.name}', '${anObject.edo_objectRef()}'))")
                 soManagedValue[anObject] = scriptObject.twin().setWeak() as? V8Object
                 return scriptObject
             }
@@ -76,9 +80,9 @@ class EDOV8ExtRuntime(val value: WeakReference<V8>) {
 
     companion object {
 
-        private val runtimeMapping: WeakHashMap<V8, EDOV8ExtRuntime> = WeakHashMap()
+        private val runtimeMapping: WeakHashMap<JSContext, EDOV8ExtRuntime> = WeakHashMap()
 
-        fun extRuntime(runtime: V8): EDOV8ExtRuntime {
+        fun extRuntime(runtime: JSContext): EDOV8ExtRuntime {
             return runtimeMapping[runtime] ?: kotlin.run {
                 val extRuntime = EDOV8ExtRuntime(WeakReference(runtime))
                 runtimeMapping[runtime] = extRuntime
